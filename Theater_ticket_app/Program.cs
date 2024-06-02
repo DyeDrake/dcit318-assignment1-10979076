355aa2cc03832ab4f4e3683a25b715e70c17cf54
Clifford Anim Boateng <aboateng_clifford@st.ug.edu.gh>
2024-06-02 17:17:03 +0000

    theater app implementation completed

---
+++ Theater_ticket_app/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class TheaterTicket
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How old are you? ");
+
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int age))
+        {
+            int ticketPrice = age switch
+            {
+                <= 12 => 7,
+                >= 65 => 7,
+                _ => 10
+            };
+            Console.WriteLine("Your ticket price is " +"GHS."+ ticketPrice);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, try again.");
+        }
+    }
+}
